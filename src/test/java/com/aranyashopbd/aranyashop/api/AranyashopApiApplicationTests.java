1b0277badc93ae71e819a5a7e11d7501262f9894
Rafi <arifhossain1572@gmail.com>
2026-02-28 16:05:01 +0600

    Initial commit: Spring Boot aranyashop-api project

---
+++ src/test/java/com/aranyashopbd/aranyashop/api/AranyashopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aranyashopbd.aranyashop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AranyashopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
